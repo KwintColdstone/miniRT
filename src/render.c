ae0940ac186f94f5a530d151bf00e9f94cc5aacc
Alisa Valiullina <avaliull@f0r4s19.codam.nl>
2026-03-06 16:51:22 +0100

    made prelimenary changes to render.c and display.c to separate image creation and render math

--- src/render.c
+++ src/render.c
@@ -135,7 +135,8 @@ static double linear_to_gamma(double linear_component)
 	return 0;
 }
 
-static void	write_color(int file, t_vec3 *pixel_color)
+// commented for now
+static void	write_color(t_vec3 *pixel_color)
 {
 	t_interval	intensity;
 
@@ -154,9 +155,9 @@ static void	write_color(int file, t_vec3 *pixel_color)
 	pixel_color->y = gbyte;
 	pixel_color->z = bbyte;
 
-	char pixel[16];
-	int len = sprintf(pixel, "%d %d %d\n", rbyte, gbyte, bbyte);
-	write(file, pixel, len);
+	//char pixel[16];
+	//int len = sprintf(pixel, "%d %d %d\n", rbyte, gbyte, bbyte);
+	//write(file, pixel, len);
 }
 
 static t_vec3	sample_square(void)
@@ -189,7 +190,7 @@ bool render(t_camera *cam, t_world *world, mlx_image_t *image)
 {
 	t_ray	r;
 	t_vec3	pixel_color;
-	int file;
+	//int file;
 	int i;
 	int j;
 	int sample;
@@ -197,25 +198,29 @@ bool render(t_camera *cam, t_world *world, mlx_image_t *image)
 
 	pixel_samples_scale = 1.0 / cam->samples_per_pixel;
 	//open file to write image to
-	file = open("image.ppm", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file == -1) {
-		write(2, "Error opening file\n", 19);
-		return (false);
-	}
+	//file = open("image.ppm", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	//if (file == -1) {
+	//	write(2, "Error opening file\n", 19);
+	//	return (false);
+	//}
 	//standard ppm start info
-	write(file, "P3\n", 3);
-	char *width = ft_itoa(cam->image_width);
-	write(file, width, ft_strlen(width));
-	write(file, " ", 1);
-	char *height = ft_itoa(cam->image_height);
-	write(file, height, ft_strlen(height));
-	write(file, "\n", 1);
-	write(file, "255\n", 4);
+	//write(file, "P3\n", 3);
+	//char *width = ft_itoa(cam->image_width);
+	//write(file, width, ft_strlen(width));
+	//write(file, " ", 1);
+	//char *height = ft_itoa(cam->image_height);
+	//write(file, height, ft_strlen(height));
+	//write(file, "\n", 1);
+	//write(file, "255\n", 4);
 	
 	//image loop
+	t_rgba	**color_array; // add malloc fail checks
+
+	color_array = ft_calloc(cam->image_height + 1, sizeof(t_rgba *));
 	i = 0;
 	while (i < cam->image_height)
 	{
+		color_array[i] = ft_calloc(cam->image_width + 1, sizeof(t_rgba));
 		fprintf(stderr,"\rScanlines remaining: %d", cam->image_height - i);
 		fflush(stderr);
 		j = 0;
@@ -230,16 +235,25 @@ bool render(t_camera *cam, t_world *world, mlx_image_t *image)
 				sample++;
 			}
 			t_vec3 final_color = multiply_by_scalar(pixel_color, pixel_samples_scale);
-			write_color(file, &final_color);
+			write_color(&final_color);
 
-			t_rgba	pixel_color;
-			pixel_color.rgba = get_color(final_color.x, final_color.y, final_color.z, 0xFF);
-			mlx_put_pixel(image, j, i, pixel_color.rgba);
-
+			color_array[i][j].rgba = get_color(final_color.x, final_color.y, final_color.z, 0xFF);
 			j++;
 		}
 		i++;
 	}
-	close(file);
+
+	i = 0;
+	while (i < cam->image_height)
+	{
+		j = 0;
+		while (j < cam->image_width)
+		{
+			mlx_put_pixel(image, j, i, color_array[i][j].rgba);
+			j++;
+		}
+		i++;
+	}
+	//close(file);
 	return (true);
 }
