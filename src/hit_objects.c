e5a6f9631045973745d1469c97112e325b3fc72d
Kwint Jongerius <kjongeri@f0r4s17.codam.nl>
2026-03-06 17:27:12 +0100

    norm hit_objects.c

--- src/hit_objects.c
+++ src/hit_objects.c
@@ -2,7 +2,8 @@
 #include "math.h"
 
 // Checks if ray hit from the inside or outside
-void	set_face_normal(const t_ray *r, const t_vec3 *outward_normal, t_hit_record *rec)
+void	set_face_normal(const t_ray *r,
+			const t_vec3 *outward_normal, t_hit_record *rec)
 {
 	rec->front_face = dot_vec3(r->direction, *outward_normal) < 0;
 	if (rec->front_face == true)
@@ -11,18 +12,21 @@ void	set_face_normal(const t_ray *r, const t_vec3 *outward_normal, t_hit_record 
 		rec->normal = multiply_by_scalar(*outward_normal, -1.0);
 }
 
-void	fill_sphere_hit_record(t_hit_record *rec, const t_sphere *s, const t_ray *r, double root)
+void	fill_sphere_hit_record(t_hit_record *rec,
+			const t_sphere *s, const t_ray *r, double root)
 {
 	t_vec3	outward_normal;
 
 	rec->t = root;
 	rec->position = ray_at(r, rec->t);
-	outward_normal = divide_by_scalar(subtract_vec3(rec->position, s->center), s->radius);
+	outward_normal = divide_by_scalar(
+			subtract_vec3(rec->position, s->center), s->radius);
 	set_face_normal(r, &outward_normal, rec);
 	rec->mat = s->mat;
 }
 
-bool	calculate_sphere_root(const t_sphere *s, const t_ray *r, t_interval ray_t, double *root)
+bool	calculate_sphere_root(const t_sphere *s,
+			const t_ray *r, t_interval ray_t, double *root)
 {
 	t_vec3	oc;
 	double	a;
@@ -33,17 +37,16 @@ bool	calculate_sphere_root(const t_sphere *s, const t_ray *r, t_interval ray_t, 
 	oc = subtract_vec3(s->center, r->origin);
 	a = vec3_len_squared(r->direction);
 	h = dot_vec3(r->direction, oc);
-	c = vec3_len_squared(oc) - s->radius*s->radius;
-	discriminant = h*h - a*c;
+	c = vec3_len_squared(oc) - s->radius * s->radius;
+	discriminant = h * h - a * c;
 	if (discriminant < 0)
 	{
 		return (false);
 	}
-	double sqrtd = sqrt(discriminant);
-	*root = (h - sqrtd) / a;
+	*root = (h - sqrt(discriminant)) / a;
 	if (*root <= ray_t.min || ray_t.max <= *root)
 	{
-		*root = (h + sqrtd) / a;
+		*root = (h + sqrt(discriminant)) / a;
 		if (*root <= ray_t.min || ray_t.max <= *root)
 			return (false);
 	}
@@ -51,75 +54,98 @@ bool	calculate_sphere_root(const t_sphere *s, const t_ray *r, t_interval ray_t, 
 }
 
 // Find nearest root in acceptable range
-bool	sphere_hit(const t_sphere *s, const t_ray *r, t_interval ray_t, t_hit_record *rec)
+bool	sphere_hit(const t_sphere *s, const t_ray *r,
+			t_interval ray_t, t_hit_record *rec)
 {
 	double	root;
+
 	if (!calculate_sphere_root(s, r, ray_t, &root))
 		return (false);
 	fill_sphere_hit_record(rec, s, r, root);
 	return (true);
 }
 
-bool	quad_hit(const t_quad *p, const t_ray *r, t_interval ray_t, t_hit_record *rec)
+// No hit if the ray is parallel to the quad
+// Return false if the hit point parameter t is outside the ray interval
+bool	calc_t(const t_ray *r, double *t, t_vec3 normal, double d)
 {
+	double	denom;
 
-	t_vec3 n;
-	t_vec3 normal;
-	double D;
-	t_vec3 w;
-	double denom;
+	denom = dot_vec3(normal, r->direction);
+	if (fabs(denom) < 1e-8)
+		return (false);
+	*t = (d - dot_vec3(normal, r->origin)) / denom;
+	return (true);
+}
+
+// Check if point is within the quad bounds (0 to 1 in both directions)
+bool	is_hit_within_quad_bounds(const t_quad *p,
+			const t_ray *r, t_vec3 w, double t)
+{
+	t_vec3		intersection;
+	t_vec3		planar_hitpt_vector;
+	double		alpha;
+	double		beta;
+	t_interval	unit_interval;
+
+	intersection = ray_at(r, t);
+	planar_hitpt_vector = subtract_vec3(intersection, p->corner);
+	alpha = dot_vec3(w, cross_vec3(planar_hitpt_vector, p->v));
+	beta = dot_vec3(w, cross_vec3(p->u, planar_hitpt_vector));
+	unit_interval = (t_interval){0, 1};
+	if (!interval_contains(unit_interval, alpha)
+		|| !interval_contains(unit_interval, beta))
+	{
+		return (false);
+	}
+	return (true);
+}
+
+bool	quad_hit(const t_quad *p, const t_ray *r,
+			t_interval ray_t, t_hit_record *rec)
+{
+	t_vec3	n;
+	t_vec3	normal;
+	double	d;
+	t_vec3	w;
+	double	t;
 
 	n = cross_vec3(p->u, p->v);
 	normal = unit_vector(n);
-	D = dot_vec3(normal, p->corner);
-	w = divide_by_scalar(n, dot_vec3(n,n));
-	denom = dot_vec3(normal, r->direction);
-	// No hit if the ray is parallel to the quad
-	if (fabs(denom) < 1e-8)
-		return false;
-
-	// Return false if the hit point parameter t is outside the ray interval
-	double t = (D - dot_vec3(normal, r->origin)) / denom;
+	d = dot_vec3(normal, p->corner);
+	w = divide_by_scalar(n, dot_vec3(n, n));
+	if (!calc_t(r, &t, normal, d))
+		return (false);
 	if (!interval_contains(ray_t, t))
 		return (false);
-
-	t_vec3 intersection = ray_at(r, t);
-	t_vec3 planar_hitpt_vector = subtract_vec3(intersection, p->corner);
-	double alpha = dot_vec3(w, cross_vec3(planar_hitpt_vector, p->v));
-	double beta = dot_vec3(w, cross_vec3(p->u, planar_hitpt_vector));
-
-	// Check if point is within the quad bounds (0 to 1 in both directions)
-	t_interval unit_interval = (t_interval){0, 1};
-	if (!interval_contains(unit_interval, alpha) || !interval_contains(unit_interval, beta))
-		return false;
-
+	if (!is_hit_within_quad_bounds(p, r, w, t))
+		return (false);
 	rec->t = t;
-	rec->position = intersection;
+	rec->position = ray_at(r, t);
 	rec->mat = p->mat;
 	set_face_normal(r, &normal, rec);
-	return  (true);
+	return (true);
 }
 
-bool	plane_hit(const t_plane *p, const t_ray *r, t_interval ray_t, t_hit_record *rec)
+bool	plane_hit(const t_plane *p, const t_ray *r,
+			t_interval ray_t, t_hit_record *rec)
 {
-	double D = dot_vec3(p->normal, p->point);
+	double	d;
+	double	denom;
+	double	t;
+	t_vec3	intersection;
 
-	double denom = dot_vec3(p->normal, r->direction);
-	// No hit if the ray is parallel to the quad
+	d = dot_vec3(p->normal, p->point);
+	denom = dot_vec3(p->normal, r->direction);
 	if (fabs(denom) < 1e-8)
-		return false;
-
-	// Return false if the hit point parameter t is outside the ray interval
-	double t = (D - dot_vec3(p->normal, r->origin)) / denom;
+		return (false);
+	t = (d - dot_vec3(p->normal, r->origin)) / denom;
 	if (!interval_contains(ray_t, t))
 		return (false);
-
-	t_vec3 intersection = ray_at(r, t);
-
+	intersection = ray_at(r, t);
 	rec->t = t;
 	rec->position = intersection;
 	rec->mat = p->mat;
 	set_face_normal(r, &p->normal, rec);
-	return  (true);
+	return (true);
 }
-
